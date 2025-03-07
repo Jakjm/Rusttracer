351d8dd4e8a46c07cc15ace03cfc4b25f128ee4c
Jordan <jakj3m@gmail.com>
2025-03-07 13:53:27 +0100

    Added function to parse command line arguments.

--- src/main.rs
+++ src/main.rs
@@ -25,37 +25,86 @@ fn test(){
     let trans = inverse.transpose();
     println!("Transpose:\n{}", trans);
 }
-fn main() -> std::io::Result<()> {
-    let args: Vec<String> = env::args().collect();
-    if args.len() < 2 {
-        println!("Input file not found!");
-    }
-    else if args[1] == "TEST"{
-        test();
-    }
-    else{
-        let file_name_index: usize;
-        //TODO parse more arguments!
-        let sample_amt: u32;
-        if args[1] == "-a" {
-            file_name_index = 2;
-            sample_amt = 8;
+fn parse_command_line_options(args: &[String]) -> Option<(&String, u32, usize)>{    
+    let mut file_name = None;
+    let mut thread_count = None;
+    let mut sample_amt = None;
+
+    let mut iter = args.iter();
+    while let Some(s) = iter.next(){
+        if *s == "-a" && sample_amt == None {
+            sample_amt = Some(8);
+        }
+        else if *s == "-t" && thread_count == None{
+            let number = match iter.next(){
+                Some(num) => num,
+                None => {
+                    println!("Please enter an integer thread count");
+                    return None;
+                },
+            };
+            let parse_result = number.to_string().trim().parse::<usize>();
+            thread_count = match parse_result{
+                Err(_e) => {
+                    println!("Please enter an integer thread count");
+                    return None;
+                },
+                Ok(num) => {
+                    if num == 0 || num > 512 {
+                        println!("Please enter a positive thread count that is at most 512.");
+                        return None;
+                    }
+                    Some(num)
+                },
+            };
+        }
+        else if file_name == None{
+            file_name = Some(s);
         }
         else{
-            file_name_index = 1;
-            sample_amt = 0;
-        }
-
-        match RenderData::read_from_file(&args[file_name_index]){
-            Ok(file_data) => {
-                println!("{}", file_data);
-                //let capacity = (file_data.width * file_data.height) as usize;
-                //let mut array = vec![self.back_color.clone(); capacity];
-                let array = file_data.render(sample_amt, 4);
-                let _ = file_data.save_image(array);
-            },
-            Err(error) => println!("{error}"),
+            println!("Could not understand command line arguments, please verify what you have entered!");
+            return None;
         }
     }
-    Ok(())
+    
+    let file_name = match file_name{
+        Some(filename) => filename,
+        None => {
+            return None;
+        },
+    };
+    let thread_count = match thread_count{
+        Some(thread_cnt) => thread_cnt,
+        None => 1,
+    };
+    let sample_amt = match sample_amt{
+        Some(sample_amt) => sample_amt,
+        None => 0,
+    };
+    return Some((&file_name, sample_amt, thread_count));
+}
+fn main() {
+    let args: Vec<String> = env::args().collect();
+    if args.len() < 2 {
+        println!("Insufficient arguments - please provide a path to an input file describing a scene.");
+        return;
+    }
+    let (filename, sample_count, thread_count) = match parse_command_line_options(&args[1..]){
+        Some((filename, sample_count, thread_count)) => (filename, sample_count, thread_count),
+        None => {
+            println!("Could not recognize command line arguments!");
+            return;
+        }
+    };
+
+    match RenderData::read_from_file(filename){
+        Ok(file_data) => {
+            println!("{}", file_data);
+            //let capacity = (file_data.width * file_data.height) as usize;
+            //let mut array = vec![self.back_color.clone(); capacity];
+            let array = file_data.render(sample_count, thread_count);
+            let _ = file_data.save_image(array);
+        },
+        Err(error) => println!("{error}"),
+    }
 }
